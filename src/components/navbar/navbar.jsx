43fb62c5f5d4c66100b0a757cd6c7c11ad0090cc
hamdymohamed1666 <m.hamdy@startupdefenders.com>
2023-12-08 00:27:02 +0200

    modal of orders

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -14,6 +14,10 @@ import PauseOutlinedIcon from '@mui/icons-material/PauseOutlined';
 import axios from '../../axios'
 import { useNavigate } from "react-router";
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import LocalPrintshopOutlinedIcon from '@mui/icons-material/LocalPrintshopOutlined';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import { Modal } from "react-bootstrap";
 const NavBar = () => {
   const [showDropDown, setShowDropDown] = useState(false);
   const router = useNavigate();
@@ -50,12 +54,50 @@ function handleLogOut()
         </div>
         <div className={classes.posPoint} style={{gap:'5px', background:'  linear-gradient(135deg, rgb(121, 241, 164) 10%, rgb(14, 92, 173) 100%)'}} onClick={()=>
         {
-      
+          setShowDropDown(true)
         }} >
        <Inventory2OutlinedIcon/>
         <span>  الطلبات </span>
         </div>
       </div>
+      <Modal size="md" show={showDropDown} onHide={() => setShowDropDown(false)}>
+        <Modal.Header closeButton>
+          الطلبات
+        </Modal.Header>
+        <Modal.Body>
+{[1,2,3,4,5,6].map((item,index)=>{
+  return <div className="PastOrder">
+  <div className="d-flex align-items-center gap-2">
+    <div className="OrderNumber">
+  #{item}
+    </div>
+    <div>
+      <h2>
+      احمد محمد
+      </h2>
+      <span>
+      21:12:39   2023-12-07 
+      </span>
+    </div>
+  
+  </div>
+  <div className="d-flex gap-2">
+ 
+  <div className="EditICon">
+  <BorderColorOutlinedIcon />
+  </div>
+  <div className="CloseIcon">
+    <CloseIcon/>
+    </div>
+  <div className="PrintIcon">
+  <LocalPrintshopOutlinedIcon />
+  </div>
+  
+  </div>
+  </div>
+})}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
